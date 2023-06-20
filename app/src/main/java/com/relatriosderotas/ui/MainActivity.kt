c8fb1dd8ce5362ced4cfc85194d96395b21f238f
Leandro-PC <leandro-pitstop@hotmail.com>
2023-06-20 17:18:01 -0300

    master: added navigation

--- app/src/main/java/com/relatriosderotas/ui/MainActivity.kt
+++ app/src/main/java/com/relatriosderotas/ui/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.relatriosderotas.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.relatriosderotas.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.relatriosderotas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
     }
 }
--- app/src/main/java/com/relatriosderotas/ui/SplashFragment.kt
+++ app/src/main/java/com/relatriosderotas/ui/SplashFragment.kt
@@ -1,40 +1,37 @@
-package com.relatriosderotas.ui.auth
+package com.relatriosderotas.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.relatriosderotas.R
-import com.relatriosderotas.databinding.FragmentLoginBinding
+import com.relatriosderotas.databinding.FragmentSplashBinding
 
-class LoginFragment : Fragment() {
+class SplashFragment : Fragment() {
 
-    private var _binding: FragmentLoginBinding? = null
+    private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initClicks()
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 2000)
     }
 
-    private fun initClicks(){
-        binding.textViewCreateNewAccount.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-        }
-
-        binding.textViewForgotPassword.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
-        }
+    private fun checkAuth(){
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
     }
 
     override fun onDestroyView() {
