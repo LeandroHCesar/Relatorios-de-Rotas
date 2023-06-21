22610383155f225f75d1445690215fbc4f61f513
Leandro-PC <leandro-pitstop@hotmail.com>
2023-06-21 20:33:03 -0300

    master: added logout, recover and frag. home

--- app/src/main/java/com/relatriosderotas/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/relatriosderotas/ui/auth/RegisterFragment.kt
@@ -1,19 +1,110 @@
 package com.relatriosderotas.ui.auth
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import com.relatriosderotas.R
+import com.relatriosderotas.databinding.FragmentLoginBinding
+import com.relatriosderotas.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+    private val database = Firebase.database
+    private val myRef = database.getReference("users")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Initialize Firebase Auth
+        auth = Firebase.auth
+        initClicks()
+        myRef.setValue("Leandro")
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d("TAG", "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException())
+            }
+        })
+    }
+
+    private fun initClicks(){
+        binding.buttonRegister.setOnClickListener { validateData() }
+    }
+
+    private fun validateData(){
+        //val name = binding.editTextNome.text.toString().trim()
+        val email = binding.editTextEmail.text.toString().trim()
+        val password = binding.editTextPassword.text.toString().trim()
+
+        if (email.isEmpty()) {
+            Toast.makeText(requireContext(), "informe seu e-mail", Toast.LENGTH_SHORT).show()
+        } else {
+            //binding.buttonRegister.isEnabled = true
+            if (password.isNotEmpty()) {
+
+                binding.progressBar.isVisible = true
+                //binding.buttonRegister.isEnabled = false
+
+                registerUser(email, password)
+            }else{
+                Toast.makeText(requireContext(), "informe seu senha", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun registerUser(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(requireContext(), "error", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
